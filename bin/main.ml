455d0c0f1acf702b2e5fb1860bb7acb7f344e5eb
Mason Fisher <masonfisherb@gmail.com>
2023-08-19 13:56:09 -0500

    Day 2 part 1

--- bin/main.ml
+++ bin/main.ml
@@ -27,16 +27,22 @@ let () =
 let () = print_newline ()
     
 let () = print_endline "Day 2 Part 1: "
-(*
 let () = 
-    let file = "./prod_1"
+    let file = "./prod_2"
     in
-    let input = open_in file in
-    let rec read_file acc = match input_line input with
-        | exception End_of_file -> acc
-        | "" -> acc
-        | line -> read_file (
-            String.get line 0 , String.get line 2 :: acc
-        ) input
+    let code_of_char start = function
+        | a -> Char.code a - Char.code start
+    in
+    let get_score c d = match (c, d) with
+        | a, b when a == (b + 1) mod  3  -> b + 1
+        | a, b when  (a + 1) mod  3  == b -> b + 7
+        | _, b -> b + 4
     in 
-    *)
+    let input = open_in file in
+    let rec read_file acc file = match input_line file with
+        | exception End_of_file -> acc
+        | line when String.length line == 3 ->  read_file ( (get_score (code_of_char 'A' (String.get line 0))  (code_of_char 'X' (String.get line 2))) + acc ) input
+        | _ -> read_file acc input
+    in 
+    print_int (read_file 0 input);
+    print_newline ()
