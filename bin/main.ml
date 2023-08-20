93d3981111711caf4dcbcca103218e921b55b318
Mason Fisher <masonfisherb@gmail.com>
2023-08-20 11:56:07 -0500

    Day 3 part 1

--- bin/main.ml
+++ bin/main.ml
@@ -64,3 +64,36 @@ let () =
     in
     print_int (read_file_2 0 input);
     print_newline ()
+
+let () = 
+    let file = "./prod_3"
+    in 
+    let input = open_in file
+    in 
+    let get_char_code c = match c with
+        | a when a >= 'a' && a <= 'z' -> Char.code a - Char.code 'a' + 1
+        | a when a >= 'A' && a <= 'Z' -> Char.code a - Char.code 'A' + 27
+        | _ -> 0
+    in
+    let find_common s1 s2 = (String.trim (String.map (fun c -> match (String.index_opt s1 c) with | Some x -> String.get s1 x | None -> ' ') s2)).[0]
+    in
+    let read_file file = 
+        let rec read_aux acc file = match input_line file with
+            | exception End_of_file -> acc
+            | line when String.length line > 2 -> let half_len = (String.length line )/2 in 
+                read_aux ((get_char_code (
+                    find_common (String.sub line 0 half_len) (String.sub line half_len ((String.length line)-half_len))
+                )) + acc) file
+            | _ -> acc
+        in 
+
+        read_aux 0 file
+    in
+    print_endline "Day 3 Part 1: ";
+    print_int (read_file input)
+
+
+
+
+
+
